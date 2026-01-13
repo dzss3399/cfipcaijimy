f86805091cdeffb62ef158469e394ed41f5b89e9
dzss3399 <182021396@qq.com>
2026-01-13 22:01:31 +0800

    Update youxuan.py

--- youxuan.py
+++ youxuan.py
@@ -15,7 +15,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 # ================= 配置 =================
 IPV4_URL = "https://www.cloudflare.com/ips-v4"
-IPS_PER_CIDR = 500
+IPS_PER_CIDR = 200
 MAX_WORKERS = 100
 TOP_N = 30
 TRACE_TIMEOUT = 4
